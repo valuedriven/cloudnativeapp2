dd7e72175cfbc0d92457788541bea02cd751d597
valuedriven <junilson@hotmail.com>
2023-02-20 11:55:02 -0300

    Configuração do Morgan

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
+const { logger } = require('./utils/logger');
 const app = express();
 const router = require('./routers/');
 
 app.use(express.json());
 
+app.use(
+  morgan('combined', { stream: { write: (message) => logger.info(message) } })
+);
 var corsOptions = {
   origin: '*',
   optionsSuccessStatus: 200,
